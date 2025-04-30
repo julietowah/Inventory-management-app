c69ea29e613e331593cf6bd8fb49633cdbfc818d
Michael4life555 <nebolisamichael8@gmail.com>
2025-04-30 15:36:56 +0100

    Updated the Login process

--- main/views.py
+++ main/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render,
+from django.contrib.auth.decorators import login_required
+# main/views.py
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from .forms import CustomLoginForm
 
-# Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        form = CustomLoginForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('dashboard')  # Change to your success URL
+    else:
+        form = CustomLoginForm()
+    return render(request, 'login.html', {'form': form})
